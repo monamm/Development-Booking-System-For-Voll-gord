1d24a22c4c78a3d0b5f97ec6fd7f20f90715f268
monamm <monamm@stud.ntnu.no>
2023-03-08 22:55:56 +0100

    Moving components and styles folder into scr folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <h3>Development of booking system for voll gård</h3>
+      <Login/>
     </div>
+    
   );
 }
 
